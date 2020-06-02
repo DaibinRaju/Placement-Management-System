9cc70154fd155f660dbf53c53f76db9a0136f6b8
joeljames270 <joeljames270@gmail.com>
2020-06-02 20:32:03 +0530

    Training Update completed

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -73,7 +73,7 @@ class TrainingController extends Controller
 
         }
 
-        return redirect('/admin/training');
+        return redirect('/admin/training')->with("success", "Training Added!");
     }
 
     /**
@@ -87,7 +87,7 @@ class TrainingController extends Controller
         $trainingdata = Training::where('id', $id)->firstOrFail();
         $dates= attendance::where('training_id',$id)->get();
         //$bill_data= training_bill::where('training_id',$id)->get();
-        return view('admin.trainingshow', compact('trainingdata','dates','bill_data'));
+        return view('admin.trainingshow', compact('trainingdata','dates'));
     }
 
     /**
@@ -98,7 +98,15 @@ class TrainingController extends Controller
      */
     public function edit($id)
     {
-        //
+        $training_data=Training::findOrFail($id);
+        $days=[];
+        for($x=1;$x<=10;$x++){
+            $days[$x]=$x;
+        }
+        $dates_load=explode(",",$training_data['dates']);
+        //dd($dates_load);
+        $days_check=$training_data['days'];
+        return view("admin.trainingedit",compact("training_data","days","days_check","dates_load"));
     }
 
     /**
@@ -110,7 +118,14 @@ class TrainingController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $training_details=Training::findOrFail($id);
+        $training_details['training_name']=$request->training_name;
+        $training_details['trainer']=$request->trainer;
+        $training_details['description']=$request->training_description;
+        $training_details['days']=$request->nSection;
+        $training_details['dates']=implode(",",$request->dates);
+        $training_details->save();
+        return redirect()->action('TrainingController@show',['id' => $id])->with('success','Training Details updated!');
     }
 
     /**
